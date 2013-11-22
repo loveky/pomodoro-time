ad5dcc8b1e574f4975d2a8b9747ac1ba2e66dafe
loveky <ylzcylx@gmail.com>
2013-11-22 16:28:09 +0800

    整理spec

--- test/controllers/pomodoroControllerSpec.js
+++ test/controllers/pomodoroControllerSpec.js
@@ -12,25 +12,31 @@ describe('controllers', function(){
       expect($scope.allTasks.length).toEqual(3);
     });
 
-    it("should add newTask to allTask if newTask's title not blank", function() {
-      $scope.newTask = {title: "test task"};
-      $scope.addTask()
-      expect($scope.allTasks.length).toEqual(4);
+    describe('#addTask', function() {
+      it("should add newTask to allTask if newTask's title not blank", function() {
+        $scope.newTask = {title: "test task"};
+        $scope.addTask()
+        expect($scope.allTasks.length).toEqual(4);
+      });
+
+      it("should not add newTask to allTask if newTask's title is blank", function() {
+        $scope.addTask()
+        expect($scope.allTasks.length).toEqual(3);
+      });
     });
 
-    it("should not add newTask to allTask if newTask's title is blank", function() {
-      $scope.addTask()
-      expect($scope.allTasks.length).toEqual(3);
+    describe('#removeTask', function() {
+      it("should remove task", function() {
+        $scope.removeTask($scope.allTasks[0]);
+        expect($scope.allTasks.length).toEqual(2);
+      });
     });
 
-    it("should remove task", function() {
-      $scope.removeTask($scope.allTasks[0]);
-      expect($scope.allTasks.length).toEqual(2);
-    });
-
-    it("should set task as activeTask", function() {
-      $scope.startTask($scope.allTasks[0]);
-      expect($scope.activeTask).toEqual($scope.allTasks[0]);
+    describe('#startTask', function() {
+      it("should set task as activeTask", function() {
+        $scope.startTask($scope.allTasks[0]);
+        expect($scope.activeTask).toBe($scope.allTasks[0]);
+      });
     });
   });
 });
